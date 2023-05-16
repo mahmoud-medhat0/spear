4f6316a15a81ab7c887801c8a139d1a9eedb9c60
mahmoud-medhat0 <hhetlar120@gmail.com>
2023-05-16 13:38:22 +0000

    Login app name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MAR EXPRESS LOGIN</title>
+    <title>{{ config('app.name'). ' '.'LOGIN' }} LOGIN</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
